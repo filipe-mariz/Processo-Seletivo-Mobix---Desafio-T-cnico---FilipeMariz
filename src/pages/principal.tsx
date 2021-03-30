a0c5f99322a83faa8f88557567ce342db8ed56fd
filipe-mariz <57136731+filipe-mariz@users.noreply.github.com>
2021-03-29 23:10:32 -0300

    Alteração na tabela

--- src/pages/principal.tsx
+++ src/pages/principal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/pages/principal.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Collapse from '@material-ui/core/Collapse';
@@ -23,26 +24,22 @@ const useRowStyles = makeStyles({
 });
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-  price: number,
+    analise: string,
+    contas: string,
+    customisacao: string,
+    financeiro: string,
 ) {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
+    analise,
+    contas,
+    customisacao,
+    financeiro,
     history: [
       { date: '2020-01-05', customerId: '11091700', amount: 3 },
       { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
     ],
 
-    analise: [
+    analised: [
         { date: '2020-01-05', customerId: '11091700', amount: 3 },
         { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
       ],
@@ -65,7 +62,7 @@ function Row(props: { row: ReturnType<typeof createData> }) {
         <TableCell component="th" scope="row">
           {row.name}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
+        <TableCell align="right">{row.analise}</TableCell>
         <TableCell align="right">{row.fat}</TableCell>
         <TableCell align="right">{row.carbs}</TableCell>
         <TableCell align="right">{row.protein}</TableCell>
@@ -110,22 +107,25 @@ function Row(props: { row: ReturnType<typeof createData> }) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+  createData('Frozen yoghurt'),
   createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
   createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
   createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
   createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+  
 ];
 
 export default function CollapsibleTable() {
   return (
-    <TableContainer component={Paper}>
-    <Table aria-label="collapsible table">
+      <div id="landing">
+          <TableContainer component={Paper}>
+          <Table aria-label="collapsible table">
         <TableHead>
           <TableRow>
+              
             <TableCell />
             <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Ver listagem</TableCell>
             <TableCell align="right">Fat&nbsp;(g)</TableCell>
             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
             <TableCell align="right">Protein&nbsp;(g)</TableCell>            
@@ -138,6 +138,9 @@ export default function CollapsibleTable() {
         </TableBody>
       </Table>
     </TableContainer> 
+      </div>
+      
+    
   );
 }
 
